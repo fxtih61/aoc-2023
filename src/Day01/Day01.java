cd1e6e6ae7331d4af8b8cabe01953df03db960d6
Fatih Tolip <fatih.tolip@cancom.de>
2023-12-12 14:58:48 +0000

    feat: für zuhause

---
+++ src/Day01/Day01.java
@@ -0,0 +1,19 @@
+package Day01;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Day01 {
+    public static void main(String[] args) throws IOException {
+        loadInput();
+    }
+
+    // Load the input file
+    public static void loadInput() throws IOException {
+        FileReader fr = new FileReader("src/Day01/day01input.csv");
+        // Suche in jeder Zeile nach einer Zahl und verkette diese zusammen.
+        // Addiere alle Zahlen von jeder Zeile zusammen und gebe das Ergebnis aus.
+
+    }
+
+}
